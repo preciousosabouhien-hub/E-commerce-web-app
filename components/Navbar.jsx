765100279358140d2013e6ef895c91592264c02d
precious <preciousosabouhien@gmail.com>
2026-09-19 14:27:27 +0100

    started working on the navigation bar

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <nav className="Navbar">
+            <div className="logo">
+                ShopEase</div>
+                <div className="nav-links">
+                    <a href="/">Home</a>
+                    <a href="/">Products</a>
+                    <a href="/">Cart</a>
+                    </div>
+                    </nav>
+    );
+}
